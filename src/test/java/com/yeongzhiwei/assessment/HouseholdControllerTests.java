c7ef95d6718e9eae25898d68b2f33be6a85461d0
yeongzhiwei <yeongzhiwei@gmail.com>
2020-10-03 15:24:06 +0800

    add test data

--- src/test/java/com/yeongzhiwei/assessment/HouseholdControllerTests.java
+++ src/test/java/com/yeongzhiwei/assessment/HouseholdControllerTests.java
@@ -50,14 +50,70 @@ public class HouseholdControllerTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private String[][] rawHouseholds = {{"LANDED"}, {"CONDOMINIUM"}, {"HDB"}};
+    private String[][] rawHouseholds = {{"LANDED"}, {"CONDOMINIUM"}, {"HDB"}, {"HDB"}, {"HDB"}, {"HDB"}};
+    private String[][][] rawFamilyMembersHouseholds = {
+            // husband/wife; income > $100k/$150k; age < 5/16 yo
+            {{"Adam", "MALE", "MARRIED", "EMPLOYED", "100000", "1980-12-12"},
+                    {"Anna", "FEMALE", "MARRIED", "EMPLOYED", "250000", "1980-05-05"},
+                    {"Angel", "FEMALE", "SINGLE", "UNEMPLOYED", "0", "2018-05-05"},
+                    {"Abbe", "FEMALE", "SINGLE", "STUDENT", "0", "2012-05-05"}},
+            // husband/wife; income < $100k/$150k; age < 16 yo
+            {{"Bob", "MALE", "MARRIED", "EMPLOYED", "25000", "1980-12-12"},
+                    {"Bella", "FEMALE", "MARRIED", "EMPLOYED", "25000", "1980-05-05"},
+                    {"Brianna", "FEMALE", "SINGLE", "UNEMPLOYED", "0", "2013-05-05"}},
+            // Husband/wife; Income < $100k/$150k
+            {{"Charlie", "MALE", "MARRIED", "EMPLOYED", "25000", "1980-12-12"},
+                    {"Cecilia", "FEMALE", "MARRIED", "EMPLOYED", "25000", "1980-05-05"}},
+            // No husband/wife; Income < $100k/$150k, age < 5/16 yo
+            {{"Dan", "MALE", "WIDOWED", "EMPLOYED", "25000", "1980-12-12"},
+                    {"Diana", "FEMALE", "SINGLE", "UNEMPLOYED", "0", "2013-05-05"}},
+            // age > 50
+            {{"Dan", "MALE", "WIDOWED", "UNEMPLOYED", "0", "1950-12-12"}},
+            // age < 50
+            {{"Ezekiel", "MALE", "WIDOWED", "UNEMPLOYED", "0", "1978-12-12"}},
+    };
+    private int getFamilyMembersLength() {
+        int length = 0;
+        for (String[][] rawFamilyMembers : rawFamilyMembersHouseholds) {
+            length += rawFamilyMembers.length;
+        }
+        return length;
+    }
 
     @BeforeEach
     void seedData() {
-        for (String[] rawHousehold : rawHouseholds) {
+        for (int i = 0; i < rawHouseholds.length; i++) {
+            String[] rawHousehold = rawHouseholds[i];
+            String[][] rawFamilyMembersHousehold = rawFamilyMembersHouseholds[i];
+
             Household household = new Household();
             household.setHousingType(HousingType.valueOf(rawHousehold[0]));
             householdRepository.save(household);
+
+            Person spouse = null;
+            for (String[] rawFamilyMember : rawFamilyMembersHousehold) {
+                Person familyMember = new Person();
+                familyMember.setName(rawFamilyMember[0]);
+                familyMember.setGender(Person.Gender.valueOf(rawFamilyMember[1]));
+                familyMember.setMartialStatus(Person.MartialStatus.valueOf(rawFamilyMember[2]));
+                familyMember.setOccupationType(Person.OccupationType.valueOf(rawFamilyMember[3]));
+                familyMember.setAnnualIncome(Integer.valueOf(rawFamilyMember[4]));
+                familyMember.setDob(LocalDate.parse(rawFamilyMember[5]));
+                familyMember.setHousehold(household);
+
+                if (rawFamilyMember[2].equals(Person.MartialStatus.MARRIED.name())) {
+                    if (spouse == null) {
+                        spouse = familyMember;
+                    } else {
+                        familyMember.setSpouse(spouse);
+                        spouse = null;
+                    }
+                }
+                Person savedPerson = personRepository.save(familyMember);
+                if (spouse != null) {
+                    spouse = savedPerson;
+                }
+            }
         }
     }
 
@@ -163,11 +219,11 @@ public class HouseholdControllerTests {
             .andExpect(jsonPath("$.id").exists())
             .andExpect(jsonPath("$.spouseId").value(person.getId()));
 
-        assertEquals(2, personRepository.count());
+        assertEquals(getFamilyMembersLength() + 2, personRepository.count());
     }
 
     @Test
-    void addFamilyMember_NonExistantHouseholdId() throws Exception {
+    void addFamilyMember_NonExistentHouseholdId() throws Exception {
         List<Long> householdIds = householdRepository.findAll()
                                     .stream()
                                     .map(Household::getId)
@@ -191,7 +247,7 @@ public class HouseholdControllerTests {
     }
 
     @Test
-    void addFamilyMember_NonExistantSpouseId() throws Exception {
+    void addFamilyMember_NonExistentSpouseId() throws Exception {
         Long validHouseholdId = householdRepository.findAll().get(0).getId();
         CreateFamilyMemberRequest request = new CreateFamilyMemberRequest();
         request.setName("Ah Lian");
@@ -207,7 +263,7 @@ public class HouseholdControllerTests {
                 .content(objectMapper.writeValueAsString(request)))
             .andExpect(status().isBadRequest());
 
-        assertEquals(0, personRepository.count());
+        assertEquals(getFamilyMembersLength(), personRepository.count());
     }
     
 }
